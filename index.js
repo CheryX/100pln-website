eab1f007cbb4b578c71a9e689dfbd612fc40fa17
CheryX <roxmb25@gmail.com>
2022-07-15 11:59:51 +0200

    Version 1.0 - It works

---
+++ index.js
@@ -0,0 +1,39 @@
+let midiParser  = require('midi-parser-js');
+let fs = require('fs');
+
+let data = fs.readFileSync('./szprotki.mid', 'base64')
+var midiArray = midiParser.parse(data);
+
+let hunderdPLN = '';
+let limit = 5000;
+
+let BPM = midiArray.timeDivision
+hunderdPLN += `!speed@${BPM}`;
+
+for (let eventList of midiArray.track) {
+    for (let event of eventList.event) {
+
+        let combineNext = false;
+
+        let delay = Math.floor(event.deltaTime/92)
+        if (delay > 100) delay = Math.floor(Math.log(delay)+90)
+        if (delay == 0) combineNext = true;
+        delay && (hunderdPLN += `|!stop@${delay}`)
+            
+        if (event.data && event.data.length == 2 && event.data[1]) {
+            
+            if (combineNext) {
+                hunderdPLN += '|!combine'
+            }
+
+            hunderdPLN += `|noteblock_harp@${event.data[0]-50}`
+
+        } 
+
+        limit --;
+        if (limit < 0) break;
+        
+    }
+}
+
+fs.writeFileSync('./musik.moai', hunderdPLN)
