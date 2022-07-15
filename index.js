9abf8ef3847baf46dd0fe5d47e958cc508b107e7
CheryX <roxmb25@gmail.com>
2022-07-15 14:24:40 +0200

    Version 1.1 - Support Multiple Instruments

--- index.js
+++ index.js
@@ -5,34 +5,121 @@ let data = fs.readFileSync('./song.mid', 'base64')
 var midiArray = midiParser.parse(data);
 
 let hunderdPLN = '';
+let instrument = '';
+let limit = 4500;
 
-// This disables "simulate realistic smoke" feature on your PC 
-let limit = 3000;
+function getEventData(event) {
+    
+    absoluteTime += event.deltaTime;
+
+    if (event.type == 255) {
+        if (event.metaType == 3) {
+            instrument = event.data;
+            return {
+                type: 'none',
+                absTime: parseDelay(absoluteTime)
+            }
+        } else {
+            return {
+                type: 'none',
+                absTime: parseDelay(absoluteTime)
+            }
+        }
+    }
+
+    if (event.type == 8) {
+        return {
+            type: 'play_note',
+            pitch: event.data[0]-70,
+            data: event.data,
+            instrument: instrument,
+            absTime: parseDelay(absoluteTime)
+        }
+    }
+
+    if (event.type == 9) {
+        return {
+            type: 'none',
+            absTime: parseDelay(absoluteTime)
+        }
+    }
+
+    return {
+        type: 'other',
+        id: event.type,
+        data: event.data,
+        instrument: instrument,
+        absTime: parseDelay(absoluteTime),
+    }
+
+}
+
+function parseDelay(delay) {
+    // Option 1:
+    return Math.floor(delay/96);
+
+    // Option 2:
+    // let newDelay = Math.floor(delay/96);
+    // if (newDelay > 15) newDelay = Math.log(newDelay) + 10
+    // return newDelay
+
+    // Option 3:
+    // return Math.log(delay)
+}
 
 let BPM = midiArray.timeDivision
 hunderdPLN += `!speed@${BPM}`;
 
-for (let eventList of midiArray.track) {
-    for (let event of eventList.event) {
+// 1. Assign every note an instrument and absolute time
+let notes = []
+let absoluteTime = 0;
 
-        let combineNext = false;
+for (instrument of midiArray.track) {
+    absoluteTime = 0;
+    
+    for (let note of instrument.event) {
 
-        // TODO: Make this part better
-        let delay = Math.floor(event.deltaTime/92)
-        if (delay > 100) delay = Math.floor(Math.log(delay)+90)
-        if (delay == 0) combineNext = true;
-        delay && (hunderdPLN += `|!stop@${delay}`)
-            
-        if (event.data && event.data.length == 2 && event.data[1]) {
-            
-            combineNext && (hunderdPLN += '|!combine')
-            hunderdPLN += `|noteblock_harp@${event.data[0]-50}`
+        let eventData = getEventData(note);
+        limit--;
 
-        } 
-
-        limit --;
-        if (limit < 0) break;
+        if (eventData.type != 'none' && limit > 0) notes.push(eventData)
         
+    }
+}
+
+// 2. Sort all of the notes in one array
+let notesSorted = notes.sort(({absTime:a}, {absTime:b}) => a-b);
+
+console.table(notesSorted)
+
+// 3. Convert it into .moai file
+
+let instruments = {
+    'Electric Piano': 'noteblock_harp',
+    'Xylophone': 'noteblock_xylophone',
+    'Grand Piano': 'noteblock_snare',
+    'Piano': 'noteblock_harp',
+    'Harpsichord': 'noteblock_guitar',
+    'Synth': 'noteblock_bass',
+    'Synth Echo': 'noteblock_bass'
+}
+
+for (let i in notesSorted) {
+    if (notesSorted[i].type == 'play_note') {
+
+        let delay = notesSorted[i].absTime - notesSorted[i-1].absTime
+        
+        //If they are supposed to be played at once
+        if (notesSorted[i].absTime == notesSorted[i-1].absTime) {
+            hunderdPLN += "|!combine"
+        } else {
+            delay && (hunderdPLN += `|!stop@${delay}`)
+        }
+
+        let instrument = instruments[notesSorted[i].instrument] 
+        if (instrument == undefined) instrument = 'noteblock_flute'
+
+        hunderdPLN += `|${instrument}@${notesSorted[i].pitch}`
     }
 }
 
